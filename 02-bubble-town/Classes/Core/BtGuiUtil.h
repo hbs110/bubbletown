59076e44cb2ce8cf5dbd6c860c6299bb61d3cc48
Gu Lu <gemfeeling@hotmail.com>
2014-12-26 09:12:05 +0800

    很小的调整

--- 02-bubble-town/Classes/Core/BtGuiUtil.h
+++ 02-bubble-town/Classes/Core/BtGuiUtil.h
@@ -24,7 +24,6 @@ public:
 class BtGuiUtil 
 {
 public: 
-    static cocos2d::MenuItemLabel* CreateMenuItem(const char* val, BtGuiListener* listener);
     static cocos2d::Menu* CreateMenu(const char* itemTexts[], int itemCnt, BtGuiListener* listener);
 };
 
