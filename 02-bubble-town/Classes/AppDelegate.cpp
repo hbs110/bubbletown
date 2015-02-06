704d861c8665df6f6ad2d9254809a606bad9e3f2
Gu Lu <gemfeeling@hotmail.com>
2015-02-06 10:53:54 +0800

    change the window to 720*1280 with scale factor 0.5

--- 02-bubble-town/Classes/AppDelegate.cpp
+++ 02-bubble-town/Classes/AppDelegate.cpp
@@ -49,7 +49,7 @@ bool AppDelegate::applicationDidFinishLaunching()
     auto director = cocos2d::Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-        glview = cocos2d::GLViewImpl::create("Cpp Empty Test");
+        glview = cocos2d::GLViewImpl::createWithRect("bubbletown", cocos2d::Rect(0, 0, 720, 1280), 0.5);
         director->setOpenGLView(glview);
     }
 
