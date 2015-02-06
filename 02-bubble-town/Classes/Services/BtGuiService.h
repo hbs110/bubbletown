b7ba4f82dfd992944200563672dab52387f5d3e9
Gu Lu <gemfeeling@hotmail.com>
2015-02-06 11:53:02 +0800

    create the bare gui service

---
+++ 02-bubble-town/Classes/Services/BtGuiService.h
@@ -0,0 +1,23 @@
+/*!
+ * \file BtGuiService.h
+ * \date 2015/02/06 11:08
+ * \author Gu Lu (gulu@kingsoft.com)
+ *
+ * \brief Definition of BtGuiService 
+*/
+
+#pragma once
+
+#include "Core/BtCoreDef.h"
+
+class BtGuiService 
+{
+    BT_SINGLETON_DEF(BtGuiService);
+
+public:
+    bool Init();
+    void Destroy();
+
+
+};
+
