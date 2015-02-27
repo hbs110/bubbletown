2cd0b79ebb103f2a1613b06f11ac2d23303af4f1
Gu Lu <gemfeeling@hotmail.com>
2015-02-27 10:16:42 +0800

    add utouch integration stubs

---
+++ 02-bubble-town/Classes/Services/BtGui_utouch.cpp
@@ -0,0 +1,15 @@
+/*!
+ * \file BtGui_utouch.cpp
+ * \date 2015/02/27 10:14
+ * \author Gu Lu (gulu@kingsoft.com)
+ *
+ * \brief Implementation of BtGui_utouch
+*/
+
+#include "stdafx.h"
+#include "BtGui_utouch.h"
+
+cocos2d::Node* BtGui_utouch::LoadLayout(const char* layoutFilePath)
+{
+    
+}
---
+++ 02-bubble-town/Classes/Services/BtGui_utouch.h
@@ -0,0 +1,16 @@
+/*!
+ * \file BtGui_utouch.h
+ * \date 2015/02/27 10:14
+ * \author Gu Lu (gulu@kingsoft.com)
+ *
+ * \brief Definition of BtGui_utouch 
+*/
+
+#pragma once
+
+class BtGui_utouch 
+{
+public: 
+    static cocos2d::Node* LoadLayout(const char* layoutFilePath);
+};
+
