b147d87f6c1306d1e6b295ddc1925435b4306d91
Gu Lu <gemfeeling@hotmail.com>
2015-03-17 10:04:21 +0800

    添加环境变量设置脚本

---
+++ set_env.py
@@ -0,0 +1,16 @@
+
+import os
+
+btjoin = os.path.join
+
+# paths
+bt_paths = dict(
+	bt_root 		= '.',
+	bt_cocos2dx 	= btjoin('.', 'external', 'cocos2d-x'),
+	bt_utouch 		= btjoin('.', 'external', 'utouch'),
+	)
+
+# apply these vars to the evironmental variables
+for k, v in bt_paths.iteritems():
+	os.environ[k] = os.path.abspath(v)
+	print '{}={}'.format(k, os.environ[k]) 
