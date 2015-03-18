b48f297cde982c6413312793591a41d864026d8a
Gu Lu <gemfeeling@hotmail.com>
2015-03-18 14:30:55 +0800

    环境变量中添加 cocos2d-x-dist

--- set_env.py
+++ set_env.py
@@ -5,10 +5,11 @@ btjoin = os.path.join
 
 # paths
 bt_paths = dict(
-	bt_root 		= '.',
-	bt_cocos2dx 	= btjoin('.', 'external', 'cocos2d-x'),
-	bt_lua 			= btjoin('.', 'external', 'BtLua-dist'),
-	bt_utouch 		= btjoin('.', 'external', 'utouch'),
+	bt_root 		 	= '.',
+	bt_cocos 			= btjoin('.', 'external', 'cocos2d-x'),
+	bt_cocos_dist 		= btjoin('.', 'external', 'cocos2d-x-dist'),
+	bt_lua 				= btjoin('.', 'external', 'BtLua-dist'),
+	bt_utouch 			= btjoin('.', 'external', 'utouch'),
 	)
 
 # apply these vars to the evironmental variables
